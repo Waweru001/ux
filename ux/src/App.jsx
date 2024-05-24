b8465a0d458895ae070ff685d81eeb7c5e2dea69
andy irungu <andy@kali.andy>
2024-05-24 10:58:13 +0300

    sss

--- ux/src/App.jsx
+++ ux/src/App.jsx
@@ -33,18 +33,20 @@ const Navbar = () => {
           <div className="w-20 rounded-full">
             <img src="https://bit.ly/4dRgYuO" />
           </div>
-        </div >
+        </div>
         <div className="flex flex-col">
-        <a className="btn btn-ghost text-xl ">Alex Mwangi</a>
-        <div className="flex justify-center items-center space-x-4">
-        <a href="https://linkedin.com/in/waweru-mwangi-8994a9231/"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-          <FaLinkedin size={24} className="fill-current" />
-        </a>
-        <Mail />
-        </div></div>
+          <a className="btn btn-ghost text-xl ">Alex Mwangi</a>
+          <div className="flex justify-center items-center space-x-4">
+            <a
+              href="https://linkedin.com/in/waweru-mwangi-8994a9231/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin size={24} className="fill-current" />
+            </a>
+            <Mail />
+          </div>
+        </div>
       </div>
 
       <div className="flex-none gap-2">
@@ -57,32 +59,34 @@ const Navbar = () => {
 const Hero = ({ scrollToAboutMe }) => {
   return (
     <div className="hero min-h-screen bg-base-200">
+      {/* <img src="./hero.svg" alt="" className="absolute bottom-0 left-0 w-32 h-32 object-cover" /> */}
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">Hello there</h1>
           <p className="py-6">
             I'M Alex a web developer with an eye for good Design.
           </p>
-          <button className="btn btn-primary flex-col " onClick={scrollToAboutMe}>
+          <button
+            className="btn btn-primary flex-col "
+            onClick={scrollToAboutMe}
+          >
             <div>Previous Work </div>
             <MdOutlineKeyboardDoubleArrowDown />{" "}
           </button>
         </div>
       </div>
-      
     </div>
   );
 };
 const AboutMe = forwardRef((props, ref) => {
   return (
-  <section ref={ref} id="about-me">
+    <section ref={ref} id="about-me">
       <div className="bg-base-200">
         <AboutMySkills />
-      </div></section>
-    
+      </div>
+    </section>
   );
 });
-
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -104,9 +108,11 @@ const Footer = () => {
         <p>Alex © {currentYear} - All rights reserved</p>
       </aside>
       <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-        <a href="https://linkedin.com/in/waweru-mwangi-8994a9231/"
-        target="_blank"
-        rel="noopener noreferrer">
+        <a
+          href="https://linkedin.com/in/waweru-mwangi-8994a9231/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaLinkedin size={24} className="fill-current" />
         </a>
         <Mail />
@@ -116,25 +122,222 @@ const Footer = () => {
   );
 };
 
+const FaqSection = () => {
+  return (
+    <div className="flex flex-col  md:space-x-8 space-y-4 justify-center items-center mb-6 mt-6">
+      <h3 className="text-center text-5xl font-bold">Experience</h3>
+      <div className="join join-vertical w-full md:w-[800px]">
+        <div className="collapse collapse-arrow join-item border border-base-300">
+          <input type="radio" name="my-accordion-4" defaultChecked />
+          <div className="collapse-title text-xl font-medium">
+            1. UI / UX Dsigner @ Digifunzi
+          </div>
+          <div className="collapse-content">
+            <p>Role</p>
+            <p>
+              <ul style={{ listStyleType: "disc", marginLeft: "1.5em" }}>
+                <li>
+                  Conducted user interviews, surveys, and usability testing to
+                  gather insights.
+                </li>
+                <li>
+                  Developed sitemaps, user flows, and wireframes to structure
+                  content.
+                </li>
+                <li>
+                  Organized and labeled content to improve navigation and
+                  findability in the LMS used by 500 + students.
+                </li>
+                <li>
+                  Created interactive prototypes to demonstrate user flows and
+                  interactions.
+                </li>
+                <li>
+                  Designed intuitive user interfaces with a focus on usability
+                  and efficiency.
+                </li>
+                <li>
+                  Planned and conducted usability tests to identify user pain
+                  points.
+                </li>
+                <li>
+                  Analyzed test results and provided actionable recommendations
+                  for improvement.
+                </li>
+                <li>
+                  Designed visually appealing interfaces that align with brand
+                  guidelines.
+                </li>
+                <li>
+                  Built high-fidelity prototypes to validate design concepts and
+                  interactions.
+                </li>
+                <li>
+                  Used tools like Figma, Sketch, InVision, and Adobe XD for
+                  prototyping.
+                </li>
+                <li>
+                  Mapped out end-to-end user journeys across multiple
+                  touchpoints.
+                </li>
+              </ul>
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow join-item border border-base-300">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-xl font-medium">
+            2. UX lead Google Developers Student's Clubs
+          </div>
+          <div className="collapse-content">
+            <p>Role</p>
+            <p>
+              <ul style={{ listStyleType: "disc", marginLeft: "1.5em" }}>
+                <li>Managed UX projects from inception to completion.</li>
+                <li>
+                  Developed sitemaps, user flows, and wireframes to structure
+                  content.
+                </li>
+                <li>
+                  Coordinated between design, development, and stakeholder teams
+                  to ensure timely delivery.
+                </li>
+                <li>
+                  Aligned UX strategies with broader customer experience goals.
+                </li>
+                <li>
+                  Technical mentor for students with interest in UX design
+                  mentoring 100 + students during my time as the UX lead
+                </li>
+                <li>
+                  Collaborated with development teams to implement designs.
+                </li>
+                <li>
+                  Lead a team of 6 Designers and Developers for a project that
+                  participated in the Global Google developer challenge.
+                </li>
+                <li>
+                  Mapped out end-to-end user journeys across multiple
+                  touchpoints.
+                </li>
+                <li>
+                  Addressed complex design challenges with creative and
+                  user-centered solutions.
+                </li>
+                <li>
+                  Presented design concepts and research findings to
+                  stakeholders.
+                </li>
+                <li>
+                  Communicated user needs and design rationale clearly and
+                  persuasively.
+                </li>
+              </ul>
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow join-item border border-base-300">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-xl font-medium">
+            3. Web Developer @ Apprentice Cloud LTD
+          </div>
+          <div className="collapse-content">
+            <p>
+              <ul style={{ listStyleType: "disc", marginLeft: "1.5em" }}>
+                <li>
+                  Conducted user interviews, surveys, and usability testing to
+                  gather insights.
+                </li>
+                <li>
+                  Developed sitemaps, user flows, and wireframes to structure
+                  content.
+                </li>
+                <li>
+                  Created interactive prototypes to demonstrate user flows and
+                  interactions.
+                </li>
+                <li>
+                  Designed intuitive user interfaces with a focus on usability
+                  and efficiency.
+                </li>
+
+                <li>
+                  Analyzed test results and provided actionable recommendations
+                  for improvement.
+                </li>
+                <li>
+                  Designed visually appealing interfaces that align with brand
+                  guidelines.
+                </li>
+                <li>
+                  Built high-fidelity prototypes to validate design concepts and
+                  interactions.
+                </li>
+                <li>
+                  Used tools like Figma, Sketch, InVision, and Adobe XD for
+                  prototyping.
+                </li>
+                <li>
+                  Used React.js to developed translations for the designs to
+                  working protoypes and products to deploy for use.
+                </li>
+                <li>
+                  Backend Development using Node.js, Express and PsSQL for
+                  adding logic and funtionality of the appications
+                </li>
+              </ul>
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow join-item border border-base-300">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-xl font-medium">
+            4. Co Founder @ WebZoom Solutions
+          </div>
+          <div className="collapse-content">
+            <p>
+              Co founded Webzoom Solutions a Web design agency with my long time
+              friend Andrew where we offer tailored Developmentsolutions for
+              businesses . <br /> These are services we offer:
+            </p>
+            <p>
+              <ul style={{ listStyleType: "decimal", marginLeft: "1.5em" }}>
+                <li>Design and ideation</li>
+                <li>Website and Mobile design and development</li>
+                <li>ERP Solutions</li>
+                <li>Custom software development.</li>
+                <li>Machine learning & AI</li>
+              </ul>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   const aboutMeRef = useRef(null);
 
   const scrollToAboutMe = () => {
     if (aboutMeRef.current) {
-      aboutMeRef.current.scrollIntoView({ behavior: 'smooth' });
+      aboutMeRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
   return (
     <div data-theme="light">
       <div>
         <Navbar />
-        <Hero scrollToAboutMe={scrollToAboutMe}/>
+        <Hero scrollToAboutMe={scrollToAboutMe} />
         {/* <section ref={aboutMeRef} id="about-me"></section> */}
-        <AboutMe ref={aboutMeRef}/>
+        <AboutMe ref={aboutMeRef} />
+        <FaqSection />
         <Footer />
       </div>
     </div>
   );
 };
+
+
 
 export default App;
